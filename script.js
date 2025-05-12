b8a3f5894499ed6b00a87badc39b95a6f9235455
chaurasiavipin <vipinchaurasia481@gmail.com>
2025-05-12 21:41:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 //complete this code
 class Rectangle {
 	constructor(width, height){
-		this._width=width;
-		this._height=height;
+		this.width=width;
+		this.height=height;
 	}
 	get width() {
-		return this._width;
+		return this.width;
 	}
 	get height(){
-return this._height;
+return this.height;
 	}
 	getArea(){
-		return this._width * this._height;
+		return this.width * this.height;
 	}
 }
 
 class Square extends Rectangle {
 	constructor(side){
 		    super(side, side);
-    this._side = side;
+    this.side = side;
 	}
 	getPerimeter(){
-		return 4 * this._side;
+		return 4 * this.side;
 	}
 }
 
