bfd32b7ec6070e2bb3b4a525a17c7d38f91a9081
chaurasiavipin <vipinchaurasia481@gmail.com>
2025-05-12 21:21:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	constructor(width, height){
+		this.width=width;
+		this.height=height;
+	}
+	get width() {
+		return this.width;
+	}
+	get height(){
+return this.height;
+	}
+	getArea():number{
+		return this.width*this.height;
+	}
+}
 
-class Square extends Animal {}
+class Square extends Animal {
+	constructor(side){
+		this.side
+	}
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
